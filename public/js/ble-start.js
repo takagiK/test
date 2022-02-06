e9ecc3a7fbc4e6290f757fa11d0672483ff0f290
takagiK <17419@g.toba-cmt.ac.jp>
2022-02-06 17:02:58 +0900

    mess

--- public/js/ble-start.js
+++ public/js/ble-start.js
@@ -1,26 +1,28 @@
-// １．BLEデバイスをスキャンする
-navigator.bluetooth.requestDevice({
-	acceptAllDevices: true, // 全てのデバイスを対象にスキャンを実施する
-	optionalServices: ['FFE0'] // 利用するServiceのUniform Type Identifierを予め指定する
-}).then(device => {
-	// ２．デバイスに接続
-	console.log('2. device', device);
-	return device.gatt.connect();
-}).then(server =>{
-	// ３-1．「Service」を指定, ServiceのUniform Type Identifierを指定
-	console.log('31. service', server);
-	return server.getPrimaryService('FFE0');
-}).then(service =>{
-	// ３-2．「Characteristc」を指定, CharacteristcのUniform Type Identifierを指定
-	console.log('32. charcteristc', charcteristc);
-	return service.getCharacteristic('FFE1');
-}).then((characteristic)  => {
-	//４．受信準備を行う
-	return characteristic.startNotifications().then(char => {
-		//５．受信したバイナリを解析、処理の実施
-		characteristic.addEventListener('characteristicvaluechanged', (event) => {
-			// 「event.target.value」がDataView型で渡ってくるのでこれを解析
-			console.log('5. event', event)
+function onClick() {
+	// １．BLEデバイスをスキャンする
+	navigator.bluetooth.requestDevice({
+		acceptAllDevices: true, // 全てのデバイスを対象にスキャンを実施する
+		optionalServices: ['FFE0'] // 利用するServiceのUniform Type Identifierを予め指定する
+	}).then(device => {
+		// ２．デバイスに接続
+		console.log('2. device', device);
+		return device.gatt.connect();
+	}).then(server =>{
+		// ３-1．「Service」を指定, ServiceのUniform Type Identifierを指定
+		console.log('31. service', server);
+		return server.getPrimaryService('FFE0');
+	}).then(service =>{
+		// ３-2．「Characteristc」を指定, CharacteristcのUniform Type Identifierを指定
+		console.log('32. charcteristc', charcteristc);
+		return service.getCharacteristic('FFE1');
+	}).then((characteristic)  => {
+		//４．受信準備を行う
+		return characteristic.startNotifications().then(char => {
+			//５．受信したバイナリを解析、処理の実施
+			characteristic.addEventListener('characteristicvaluechanged', (event) => {
+				// 「event.target.value」がDataView型で渡ってくるのでこれを解析
+				console.log('5. event', event)
+			});
 		});
 	});
-});
+}
